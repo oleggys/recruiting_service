47db428f546ba1c14a9fe2ad7e8e92b766791561
GysevOV <gysevov@yandex.ru>
2020-01-12 22:31:40 +0300

    update settings

--- recruiting_service/settings.py
+++ recruiting_service/settings.py
@@ -143,8 +143,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = 'static'
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
+STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
-]
+)
